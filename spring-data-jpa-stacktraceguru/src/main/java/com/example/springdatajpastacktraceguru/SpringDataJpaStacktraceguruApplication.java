2cb8cb8c9bdffe2fc4811096afd7fc568b3923ed
moueza <peter@oscillan1>
2020-09-06 08:13:25 +0200

    start

---
+++ spring-data-jpa-stacktraceguru/src/main/java/com/example/springdatajpastacktraceguru/SpringDataJpaStacktraceguruApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpastacktraceguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStacktraceguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaStacktraceguruApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa-stacktraceguru/src/test/java/com/example/springdatajpastacktraceguru/SpringDataJpaStacktraceguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpastacktraceguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaStacktraceguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
